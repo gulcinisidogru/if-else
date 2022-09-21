cea9512729ee5e306a0df17dd9b8f48cc9ee394b
gulci <gulci@LAPTOP-KPNR185M>
2022-09-21 17:58:12 +0300

    commit

---
+++ src/ifElse/main.java
@@ -0,0 +1,17 @@
+package ifElse;
+
+public class main {
+
+	public static void main(String[] args) {
+		int sayi=40;  
+		if(sayi<20){
+			System.out.println("sayi 20'den küçüktür");
+		}else if (sayi==20){
+			System.out.println("sayi 20 ye eşittir");
+		}else {
+			System.out.println("sayi 20'den büyüktür");
+		}
+		
+
+}
+}
